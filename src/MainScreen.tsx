2b18b480a3c46f8cbe4ae639d355e647b42d1aed
anasAB <anas.222@hotmail.com>
2024-06-05 16:11:00 +0200

    store watched videos in localStorage

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -5,9 +5,10 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
+import Badge from '@mui/material/Badge';
 
 export interface IVideoItem {
+    id: number
     title: string;
     thumbnail: string;
     videoUrl: string;
@@ -16,42 +17,63 @@ export interface IVideoItem {
 
 const MainScreen = () => {
 
-    const [data, setData] = useState<IVideoItem[]>([]);
-    const navigate = useNavigate();
+    const navigate = useNavigate()
+
+
+    const [data, setData] = useState<IVideoItem[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch('/videos.json')
-            .then(response => response.json())
-            .then(fetchedData => setData(fetchedData))
-            .catch(error => console.error("Failed to fetch data:", error));
-    }, []);
+           .then(response => {
+                if (!response.ok) throw new Error('Network response was not ok');
+                return response.json();
+            })
+           .then(fetchedData => {
+                setData(fetchedData);
+                setLoading(false);
+            })
+           .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, [])
 
     const playVidoHandler = (video: IVideoItem) => navigate('/videoplayer', { state: { video } })
 
+    const watchedPlaysVideoId =  localStorage.getItem('videoIds')
+
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error: {error}</div>
+
     return (
         <div className="grid grid-cols-3 gap-4 p-4 flex flex-wrap justify-center">
-            {data.map((video, index) => (
-                <Card sx={{ maxWidth: 345, margin: '0 auto', marginBottom: '20px' }} key={index}>
-                    <CardActionArea onClick={() => playVidoHandler(video)}>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image={video.thumbnail}
-                            alt={video.title}
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {video.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            ))}
-
+           {data.map((video) => (
+            <Card sx={{ maxWidth: 345, margin: '0 auto', marginBottom: '20px' }} key={video.id}>
+                
+                <CardActionArea onClick={() => playVidoHandler(video)}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={video.thumbnail}
+                        alt={video.title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {video.title}
+                           
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                           
+                            {watchedPlaysVideoId && watchedPlaysVideoId.includes(String(video.id)) && <Badge badgeContent="Watched" color="success"/> }
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        ))}
         </div>
     );
 
@@ -59,6 +81,4 @@ const MainScreen = () => {
 
 
 
-export default MainScreen;
-
-// className='w-64 p-4 rounded-lg bg-three flex flex-col space-y-4' flex flex-col space-y-2
+export default React.memo(MainScreen);
--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
 import { useNavigate, useLocation } from 'react-router-dom';
 
@@ -7,23 +7,44 @@ const VideoPlayer = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
-  
+
+
   const video = location.state.video;
+  const { title, videoUrl, id } = video
 
-  const { title, videoUrl } = video
+  const onLoadHandler = (e:any): void => setCurrentPlaysVideoId(e.target.id)
+  
+    // Initialize state from local storage or start with an empty array
+    const storedVideoIds = JSON.parse(localStorage.getItem('videoIdsArray') || '[]');
+    const [currentPlaysVideoId, setCurrentPlaysVideoId] = useState<number>(0);
 
-  const test = (test:any) => {
-    console.log('test',test);
+ 
+
     
-  }
+    useEffect(() => {
+      // Check if the ID is not already in the array before adding it
+      // const newVideoIds = storedVideoIds.includes(currentPlaysVideoId) ? storedVideoIds : [...storedVideoIds, currentPlaysVideoId];
+      const newVideoIds = [...storedVideoIds, currentPlaysVideoId];
+
+      // Update the local storage with the new array of video IDs
+      localStorage.setItem('videoIdsArray', JSON.stringify(newVideoIds));
+
+    }, [currentPlaysVideoId, storedVideoIds]);
+
+    // // Effect to update local storage whenever currentPlaysVideoId changes
+    // useEffect(() => {
+    //   const newVideoIds = storedVideoIds.includes(currentPlaysVideoId)? storedVideoIds : [...storedVideoIds, currentPlaysVideoId];
+    //   // Update the local storage with the new array of video IDs
+    //   localStorage.setItem('videoIds', JSON.stringify(newVideoIds));
+    // }, [currentPlaysVideoId]);
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-gray-600">
-        {title}
-      </h1>
-
+      <h1 className="text-3xl font-bold text-gray-600"> {title} </h1>
       <iframe
+        id={id}
+        onLoad={(e) => onLoadHandler(e)}
         width={1000}
         height={500}
         loading='lazy'
@@ -31,16 +52,11 @@ const VideoPlayer = () => {
         title={title}
         allowFullScreen
         className=" rounded-lg shadow-lg"
-        onCanPlay={(e) => (test(e))}
-        onPlay={(e) => test(e)}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"
       />
 
-      {/* <iframe  src={videoUrl} width={1000} height={500} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"></iframe> */}
+      <Button variant="contained" onClick={() => navigate('/', { state: { currentPlaysVideoId } })}>Main Screen</Button>
 
-
-
-      <Button variant="contained" onClick={() => navigate('/')}>Main Screen</Button>
     </div>
   );
 }
